796c42fc4638f71fa47548cced3a4eba5ec5adc0
PRATECH\ssalazar <ssalazar@pratechgroup.com>
2020-06-21 20:15:23 -0500

    Refactor General

--- src/pages/Exercises.js
+++ src/pages/Exercises.js
@@ -1,18 +1,14 @@
-import React, { Fragment } from 'react'
-import Welcome from '../components/Welcome'
-import ExerciseList from '../components/ExerciseList'
-import AddButtom from '../components/AddButton'
+import React, { Fragment } from 'react';
+import Welcome from '../components/Welcome';
+import ExerciseList from '../components/ExerciseList';
+import AddButtom from '../components/AddButton';
 
-const Exercises = ({data}) => (
-    <Fragment>
-        <Welcome 
-            username="Raúl"
-        />
-        <ExerciseList 
-            exercises={data}
-        />
-        <AddButtom />
-    </Fragment>
-)
+const Exercises = ({ data }) => (
+	<Fragment>
+		<Welcome username="Santiago" />
+		<ExerciseList exercises={data} />
+		<AddButtom />
+	</Fragment>
+);
 
-export default Exercises
+export default Exercises;
